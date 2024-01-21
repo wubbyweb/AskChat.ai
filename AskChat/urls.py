541fafa3c6b9f25da14153011024837fb1140404
Rajesh G <rajgoaj@gmail.com>
2024-01-21 00:01:02 -0600

    Added model engine, result page

--- AskChat/urls.py
+++ AskChat/urls.py
@@ -20,4 +20,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
+    path("result/", views.result,name='result'),
 ]
--- AskChat/views.py
+++ AskChat/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from . import fulfill
 
 def home(request):
     return render(request,'index.html')
+
+def result(request):
+    user_ask_value = request.GET["user_ask_inputbox"]
+    ml_response = fulfill.model_response()
+    return render(request,'result.html',{'model_response':ml_response})
