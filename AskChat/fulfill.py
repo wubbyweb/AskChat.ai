542a27c5d171eeb24c3287449fcb1b1c393d4a44
Rajesh G <rajgoaj@gmail.com>
2024-02-02 01:08:02 -0600

    Added user input feature

--- AskChat/fulfill.py
+++ AskChat/fulfill.py
@@ -1,13 +1,13 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 from . import urls
 
-def model_response():
+def model_response(user_ask_value):
     model_name = "timpal0l/mdeberta-v3-base-squad2"
 
     # a) Get predictions
     nlp = pipeline('question-answering', model=model_name, tokenizer=model_name)
     QA_input = {
-        'question': 'What this say about good debt?',
+        'question': user_ask_value,
         'context': urls.startup()
     }
     res = nlp(QA_input)
--- AskChat/views.py
+++ AskChat/views.py
@@ -6,5 +6,5 @@ def home(request):
 
 def result(request):
     user_ask_value = request.GET["user_ask_inputbox"]
-    ml_response = fulfill.model_response()
+    ml_response = fulfill.model_response(user_ask_value)
     return render(request,'result.html',{'model_response':ml_response})
